d1b9b83518a6e907f398f7ed43b4a5282c736673
taoguangc <taoguangc@gmail.com>
2025-02-13 16:29:37 +0800

    add src/const.ts fix rss.xml.ts error

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = '极客创业'
+export const SITE_DESCRIPTION = '专注于技术创新和创业'
+export const SITE_URL = 'https://geek-startup.com'
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,20 +1,11 @@
 import rss from '@astrojs/rss'
-import { getCollection } from 'astro:content'
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts'
-import { sortItemsByDateDesc } from '../utils/data-utils'
 
-export async function GET(context: { site: URL }) {
-  const posts = (await getCollection('posts')).sort(sortItemsByDateDesc)
-
+export async function GET(context) {
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      description: post.data.excerpt,
-      link: `/posts/${post.slug}/`,
-      pubDate: post.data.publishDate
-    }))
+    items: []
   })
 }
